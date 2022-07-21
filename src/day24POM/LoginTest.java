3d4c5aec38278e89561cae8a0e70ae6bb8b74fda
unknown <ranita.saha4@yahoomail.com>
2022-07-21 14:13:32 +0200

    modifications

---
+++ src/day24POM/LoginTest.java
@@ -0,0 +1,27 @@
+package day24POM;
+//change site
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class LoginTest {
+
+	WebDriver driver;
+	
+	@BeforeClass
+	void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "E://selenium//chromedriver_win32/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/");
+	}
+	
+	void login()
+	{
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName("mercury");
+		lp.setPassword("mercury");
+		lp.clickSubmit();
+	}
+}
